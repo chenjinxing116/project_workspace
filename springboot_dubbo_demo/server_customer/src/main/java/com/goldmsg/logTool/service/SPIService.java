fad7e27d8fd03d2406914b714ff0fbb2ef71c8f1
chenjinxing116 <1272635875@qq.com>
2020-08-05 17:13:10 +0800

    M 删除多余项目

--- springboot_dubbo_demo/server_customer/src/main/java/com/goldmsg/logTool/service/SPIService.java
+++
@@ -1,6 +0,0 @@
-package com.goldmsg.logTool.service;
-
-public interface SPIService {
-    void execute();
-
-}
--- springboot_dubbo_demo/server_provider/src/main/java/com/goldmsg/logTool/service/TokenService.java
+++
@@ -1,8 +0,0 @@
-package com.goldmsg.logTool.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface TokenService {
-    String createToken();
-    Boolean checkToken(HttpServletRequest request) throws Exception;;
-}
