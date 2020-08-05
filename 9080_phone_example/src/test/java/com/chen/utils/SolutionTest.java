c2d6a1305a03311c43b34b85aed6e9eb2c081382
chenjinxing116 <1272635875@qq.com>
2020-08-05 11:50:33 +0800

    更新测试用例

--- 9080_phone_example/src/test/java/com/chen/utils/SolutionTest.java
+++ 9080_phone_example/src/test/java/com/chen/utils/SolutionTest.java
@@ -8,8 +8,9 @@ import java.util.List;
 public class SolutionTest {
 
 
-    @Test
+    @Test(timeout = 100)
     public void letterCombinations() {
+        long startTime = System.currentTimeMillis();
         Solution s = new Solution();
         List l = new ArrayList();
         l.add("1");
@@ -19,6 +20,10 @@ public class SolutionTest {
             input = input.append(l.get(i));
         }
         List<String> ls = s.letterCombinations(input.toString());
+        long endTime = System.currentTimeMillis();
+        float seconds = (endTime - startTime) / 1000F;
+        //输入letterCombinations方法执行时间
+        System.out.println("letterCombinations()方法执行时间（单位秒）：" + Float.toString(seconds) + " seconds.");
         System.out.println(input.toString());
         System.out.println(ls.toString());
     }
